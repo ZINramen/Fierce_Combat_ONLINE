98d794f99f90ffbe985ae87f5c74b4b4c51051c3
choimingue-game <112677582+choimingue-game@users.noreply.github.com>
2023-09-24 23:24:36 +0900

    2023.09.24 Add Anim
    동키콩 애니메이션 적용및 형태 구현 완료

---
+++ Assets/Mingyu_TestAsset/Donkey Kong/DK_Ctrl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DK_Ctrl : MonoBehaviour
+{
+    public bool Is_Roll;
+    private Animator myAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Is_Roll)
+            myAnim.SetBool("Is_Roll", true);
+        else
+            myAnim.SetBool("Is_Roll", false);
+    }
+}
