f8fd60d7045e18311e57791c2248a3684baba4d6
choimingue-game <112677582+choimingue-game@users.noreply.github.com>
2023-11-05 21:47:00 +0900

    2023.11.05 Anim 세팅 끝
    전체적인 애니메이션 세팅 끝
    
    문제점
    플레이어가 생성되지 않음

--- Assets/02_Scripts/Manager/Video_Ctrl.cs
+++ Assets/02_Scripts/Manager/Video_Ctrl.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Video_Ctrl : MonoBehaviour
 {
+    public GameObject Video_Obj;
     public GameObject videoPlayer;
     public GameObject Computer;
     public GameObject GameStartPannel;
 
     private GameObject mainCam;
     private UnityEngine.Video.VideoPlayer VideoComponent;
+
+    private bool is_StartVideo = false;
+
     private const float WaitTime = 1f;
     private float WaitCount = 0f;
 
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         VideoComponent = videoPlayer.GetComponent<UnityEngine.Video.VideoPlayer>();
         mainCam = GameObject.Find("Main Camera");
@@ -22,13 +27,23 @@ public class Video_Ctrl : MonoBehaviour
 
     private void Update()
     {
-        WaitCount += Time.deltaTime;
+        if (mainCam.GetComponent<Cam_AnimCtrl>().Get_isEndAnim() 
+            && is_StartVideo == false)
+        {
+            is_StartVideo = true;
+
+            VideoComponent.Play();
+            Video_Obj.SetActive(true);
+        }
+
+        else if(is_StartVideo)
+            WaitCount += Time.deltaTime;
 
         if (VideoComponent.isPlaying == false && WaitCount >= WaitTime)
         {
             mainCam.GetComponent<Camera>().clearFlags = CameraClearFlags.Skybox;
-            Computer.SetActive(false);
             GameStartPannel.SetActive(true);
+            Video_Obj.SetActive(false);
             this.gameObject.SetActive(false);
         }
     }
