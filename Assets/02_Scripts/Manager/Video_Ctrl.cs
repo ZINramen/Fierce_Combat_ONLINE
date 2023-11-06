d9ba947f5a5f766ac88838caa7688095639fa1ca
Zinramen <fhgh6290@gmail.com>
2023-11-06 20:11:46 +0900

    Revert "2023.11.05 Anim 세팅 끝"
    This reverts commit f8fd60d7045e18311e57791c2248a3684baba4d6.

--- Assets/02_Scripts/Manager/Video_Ctrl.cs
+++ Assets/02_Scripts/Manager/Video_Ctrl.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class Video_Ctrl : MonoBehaviour
 {
-    public GameObject Video_Obj;
     public GameObject videoPlayer;
     public GameObject Computer;
     public GameObject GameStartPannel;
 
     private GameObject mainCam;
     private UnityEngine.Video.VideoPlayer VideoComponent;
-
-    private bool is_StartVideo = false;
-
     private const float WaitTime = 1f;
     private float WaitCount = 0f;
 
-
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
         VideoComponent = videoPlayer.GetComponent<UnityEngine.Video.VideoPlayer>();
         mainCam = GameObject.Find("Main Camera");
@@ -27,23 +22,13 @@ public class Video_Ctrl : MonoBehaviour
 
     private void Update()
     {
-        if (mainCam.GetComponent<Cam_AnimCtrl>().Get_isEndAnim() 
-            && is_StartVideo == false)
-        {
-            is_StartVideo = true;
-
-            VideoComponent.Play();
-            Video_Obj.SetActive(true);
-        }
-
-        else if(is_StartVideo)
-            WaitCount += Time.deltaTime;
+        WaitCount += Time.deltaTime;
 
         if (VideoComponent.isPlaying == false && WaitCount >= WaitTime)
         {
             mainCam.GetComponent<Camera>().clearFlags = CameraClearFlags.Skybox;
+            Computer.SetActive(false);
             GameStartPannel.SetActive(true);
-            Video_Obj.SetActive(false);
             this.gameObject.SetActive(false);
         }
     }
