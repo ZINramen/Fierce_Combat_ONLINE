ec21f97c277c59771c4f012fcf2b0576dbe01e7f
choimingue-game <112677582+choimingue-game@users.noreply.github.com>
2023-11-21 18:07:12 +0900

    2023.11.21
    Test_Commit
    민규 폴더에 스크립트들 추가

---
+++ Assets/Mingyu/02_Scripts/Photon_Menu/Mingyu_RoomCtrl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class Mingyu_RoomCtrl : MonoBehaviourPunCallbacks
+{
+    public Text roomNameT;
+    public Text playerNumberT;
+    public Text StageNameT;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
