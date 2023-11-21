65d0a8a86a6ba35f1426e4f4c5700cbc8c4dcb76
choimingue-game <112677582+choimingue-game@users.noreply.github.com>
2023-11-21 23:17:04 +0900

    23.11.21 인트로 + 메인 화면 버튼 조작및 채팅 구현

---
+++ Assets/Mingyu/02_Scripts/Photon_Menu/Mingyu_Goto_TitleScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mingyu_Goto_TitleScene : MonoBehaviour
+{
+    public void Goto_TitleScene()
+    {
+        SceneManager.LoadScene("TitleMenu(Demo)");
+    }
+}
