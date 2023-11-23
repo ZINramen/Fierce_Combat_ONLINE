afbd7ed7b6430dfaabe33b181ce3081d9763a65e
20191332 김호현 <fhgh6290@gmail.com>
2023-11-23 15:36:04 +0900

    Camera

---
+++ Assets/Hyun/Scripts/DynamicCamera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicCamera : MonoBehaviour
+{
+    public float offsetX = 0;
+    public float offsetY = 0;
+    public float offsetZ = 0;
+
+    public Transform Player1;
+    public Transform Player2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
