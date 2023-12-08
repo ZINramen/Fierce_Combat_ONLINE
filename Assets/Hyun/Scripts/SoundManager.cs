21b48e8511b019ea79aaa266b39bc3e508186ac1
Zinramen <fhgh6290@gmail.com>
2023-12-08 13:15:36 +0900

    검 스킬 필살기 추가

--- Assets/Hyun/Scripts/SoundManager.cs
+++ Assets/Hyun/Scripts/SoundManager.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    AudioSource sound;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sound = GetComponent<AudioSource>();   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PlaySound(AudioClip clip) 
+    {
+        sound.PlayOneShot(clip);
     }
 }
